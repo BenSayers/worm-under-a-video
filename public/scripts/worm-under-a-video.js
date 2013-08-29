2583f6618e2d4c464ce5491e88a80466ed7a05fa
garethrhughes <gareth@grhughes.com>
2013-08-29 15:26:00 +1000

    Adding basic slider and graph code

--- public/scripts/worm-under-a-video.js
+++ public/scripts/worm-under-a-video.js
@@ -1,1 +1,74 @@
-console.log('well that worked!');
+$(function() {
+    $( "#slider-vertical" ).slider({
+        orientation: "vertical",
+        range: "min",
+        min: 0,
+        max: 10,
+        value: 5,
+        step: 1,
+
+        slide: function( event, ui ) {
+            console.log(ui.value);
+        }
+    });
+
+
+    $('#container').highcharts({
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: null
+        },
+        xAxis: {
+            type: 'seconds'
+        },
+        yAxis: {
+            title: {
+                text: 'Sentiment'
+            },
+            min: 0,
+            max: 10,
+            minorGridLineWidth: 0,
+            gridLineWidth: 0,
+            alternateGridColor: null,
+            plotBands: [{
+                from: 0,
+                to: 5,
+                color: 'rgba(255, 0, 0, 0.1)'
+            }, {
+                from: 5,
+                to: 10,
+                color: 'rgba(0, 255, 0, 0.1)'
+            }]
+        },
+        tooltip: {
+            valueSuffix: ' m/s'
+        },
+        plotOptions: {
+            spline: {
+                lineWidth: 4,
+                states: {
+                    hover: {
+                        lineWidth: 5
+                    }
+                },
+                marker: {
+                    enabled: false
+                },
+                pointInterval: 1,
+                pointStart: 0
+            }
+        },
+        series: [{
+            data: [5, 4, 3, 2, 1, 0, 0, 0, 0, 5, 6, 7, 8, 9, 10, 10, 10, 1, 2, 3, 4, 5, 5, 5, 5, 5, 4, 3, 2, 2, 2, 5, 4, 3, 2, 1, 0, 0, 0, 0, 5, 6, 7, 8, 9, 10, 10, 10, 1, 2, 3, 4, 5, 5, 5, 5, 5, 4, 3, 2, 2, 2]
+
+        }]
+        ,
+        navigation: {
+            menuItemStyle: {
+                fontSize: '10px'
+            }
+        }
+    });
+});
